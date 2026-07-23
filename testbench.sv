0508ae450c979963b9cae13d2962597f608de416
Gagan Krishna S <140827228+gagankrishnaa@users.noreply.github.com>
2026-07-23 23:58:19 +0530

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -1,10 +1,14 @@
 module uart_tx_test;
   reg clk, rst, start;
   reg [7:0] data_in;
-  wire tick, tx, busy;
+  wire [2:0] count;
+  wire tick, tx, busy, data_ready;
+  wire [7:0] data_out;
   
-  baud_gen bg (.clk(clk), .rst(rst), .tick(tick));
+  
+  baud_gen bg (.clk(clk), .rst(rst), .tick(tick), .count(count));
   uart_tx uut (.clk(clk), .rst(rst), .tick(tick), .start(start), .data_in(data_in), .tx(tx), .busy(busy));
+  uart_rx uuy (.clk(clk), .rst(rst), .tick(tick), .count(count), .rx(tx), .data_out(data_out), .data_ready(data_ready));
   
   initial begin
     clk = 0;
@@ -22,14 +26,13 @@ module uart_tx_test;
     #12 start = 1;
     #46 start = 0;
   end
-
+  
   initial begin
-    #500;
-    $finish;
+    #900 $finish;
   end
   
   initial begin
     data_in = 8'b10110010;
-    $monitor ("$time=%0t clk=%b rst=%b tick=%b start=%b data_in=%b tx=%b busy=%b", $time, clk,rst, tick, start, data_in, tx, busy);
+    $monitor ("$time=%0t clk=%b rst=%b tick=%b start=%b data_in=%b tx=%b busy=%b data_out=%b data_ready=%b count=%b", $time, clk,rst, tick, start, data_in, tx, busy, data_out, data_ready, count);
   end
 endmodule
